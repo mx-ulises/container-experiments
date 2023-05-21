736ae89a31ff9a75a7a92151e03a28e01f307c36
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-05-21 11:16:06 -0700

    Kubernetes - Build a simple app that is exposed on the internet

--- simple-kubernetes-flask-app/simple-flask-app/app.py
+++ simple-kubernetes-flask-app/simple-flask-app/app.py
@@ -3,10 +3,15 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+HOSTNAME = os.environ.get('HOSTNAME', 'Default')
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def home():
+    return f'{HOSTNAME} - 我喜欢中国菜'
+
+@app.route('/ready')
+def ready():
+    return 'Ready'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
