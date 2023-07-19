7a7dd6844667d9b11f7688651c88c08f03175009
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-07-18 22:54:35 -0700

    Prometheus: Up & Running - Update Go App to use Gauges and allow different request types: Save and Spend

--- prometheus-up-and-running/ch3/main.go
+++ prometheus-up-and-running/ch3/main.go
@@ -12,35 +12,75 @@ import (
 )
 
 var (
-	requestCount = prometheus.NewCounter(
+	saveRequestCount = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "puar_golang_http_request_count",
-			Help: "Total number of HTTP requests received in this Golang App",
+			Name: "puar_golang_save_http_request_count",
+			Help: "Total number of save HTTP requests received in this Golang App",
 		},
 	)
 
-	requestErrorCount = prometheus.NewCounter(
+	saveRequestErrorCount = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "puar_golang_http_request_error_count",
-			Help: "Total number of HTTP requests errors received in this Golang App",
+			Name: "puar_golang_save_http_request_error_count",
+			Help: "Total number of save HTTP requests errors received in this Golang App",
 		},
 	)
 
-	coinCount = prometheus.NewCounter(
+	savedCoinCount = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "puar_golang_http_coin_count",
-			Help: "Total number of coins reived in request",
+			Name: "puar_golang_saved_coin_count",
+			Help: "Total number of coins saved",
+		},
+	)
+
+	spendRequestCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "puar_golang_spend_http_request_count",
+			Help: "Total number of spend HTTP requests received in this Golang App",
+		},
+	)
+
+	spendRequestErrorCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "puar_golang_spend_http_request_error_count",
+			Help: "Total number of spend HTTP requests errors received in this Golang App",
+		},
+	)
+
+	spentCoinCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "puar_golang_spent_coin_count",
+			Help: "Total number of coins spent",
+		},
+	)
+
+	coinBalance = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "puar_golang_coin_balance",
+			Help: "Current balance of coins in this app (Saved - Spent)",
 		},
 	)
 )
 
 func init() {
-	prometheus.MustRegister(requestCount)
-	prometheus.MustRegister(requestErrorCount)
-	prometheus.MustRegister(coinCount)
+	prometheus.MustRegister(saveRequestCount)
+	prometheus.MustRegister(saveRequestErrorCount)
+	prometheus.MustRegister(savedCoinCount)
+	prometheus.MustRegister(spendRequestCount)
+	prometheus.MustRegister(spendRequestErrorCount)
+	prometheus.MustRegister(spentCoinCount)
+	prometheus.MustRegister(coinBalance)
 }
 
-func requestCounterHandler(w http.ResponseWriter, r *http.Request) {
+func generalHandler(
+	requestCount prometheus.Counter,
+	requestErrorCount prometheus.Counter,
+	coinCount prometheus.Counter,
+	operation string,
+	w http.ResponseWriter,
+	r *http.Request,
+	sign float64,
+) {
 	// Increase request count
 	requestCount.Inc()
 
@@ -63,6 +103,9 @@ func requestCounterHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	if coins <= 0 {
+		panic("Negative number of coins provided")
+	}
 
 	// Evaluate Random success rate at 20%
 	rand.Seed(time.Now().UnixNano())
@@ -73,13 +116,23 @@ func requestCounterHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Increase number of coins
 	coinCount.Add(coins)
+	coinBalance.Add(coins * sign)
 
 	// Successful request
-	fmt.Fprint(w, "Successful Request, added: ", coins)
+	fmt.Fprint(w, "Successful ", operation, " Request, coins: ", coins)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	generalHandler(saveRequestCount, saveRequestErrorCount, savedCoinCount, "Save", w, r, 1)
+}
+
+func spendHandler(w http.ResponseWriter, r *http.Request) {
+	generalHandler(spendRequestCount, spendRequestErrorCount, spentCoinCount, "Spend", w, r, -1)
 }
 
 func main() {
-	http.HandleFunc("/counter", requestCounterHandler)
+	http.HandleFunc("/save", saveHandler)
+	http.HandleFunc("/spend", spendHandler)
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
